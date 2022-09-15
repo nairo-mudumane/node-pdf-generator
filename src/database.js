7d483e1164ef4c44d7a140d3d187ee68ad9a56c5
nairo-mudumane <nairomudumane@outlook.com>
2022-09-15 14:15:20 +0200

    created database and pdf templete

---
+++ src/database.js
@@ -0,0 +1,21 @@
+const passengers = [
+  {
+    name: "Joyce",
+    flightNumber: 7569,
+    time: "12:30",
+  },
+  {
+    name: "Nairo Mudumane",
+    flightNumber: 4521,
+    time: "04:30",
+  },
+  {
+    name: "John Wick",
+    flightNumber: 9534,
+    time: "10:45",
+  },
+];
+
+module.exports = {
+  passengers,
+};
--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+const db = require("./database");
 const app = express();
 const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(cors({ origin: "*" }));
 
 app.listen(PORT, () => console.log(`running on : ${PORT}`));
